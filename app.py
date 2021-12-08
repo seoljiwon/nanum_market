4e92ddad7f9157995d8a158bebc09afa163c5286
seol <suljiwon@naver.com>
2021-12-09 02:07:22 +0900

    feat: post create, post detail

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask.helpers import url_for
 import psycopg2
 import os
 from dotenv import load_dotenv
+from werkzeug.utils import secure_filename
 
 load_dotenv(verbose=True)
 
@@ -17,20 +18,20 @@ connect = psycopg2.connect(DB_CONFIG)
 cur = connect.cursor()
 
 # 테이블 생성
-cur.execute('DROP TABLE user_like;')
-cur.execute('DROP TABLE post_like;')
-cur.execute('DROP TABLE comment;')
-cur.execute('DROP TABLE buy;')
-cur.execute('DROP TABLE post;')
-cur.execute('DROP TABLE users;')
+# cur.execute('DROP TABLE user_like;')
+# cur.execute('DROP TABLE post_like;')
+# cur.execute('DROP TABLE comment;')
+# cur.execute('DROP TABLE buy;')
+# cur.execute('DROP TABLE post;')
+# cur.execute('DROP TABLE users;')
 
-cur.execute('CREATE TABLE users (id varchar(20), password varchar(20), phone numeric(11, 0), phone_public boolean, primary key(id));')
-cur.execute('CREATE TABLE post (id SERIAL, user_id varchar(20), title varchar(20) not null, content text, location text, image_urls text, created_at timestamp default now(), updated_at timestamp default now(), is_closed boolean, primary key(id), foreign key (user_id) references users on delete cascade);')
-cur.execute('CREATE TABLE comment (id SERIAL, user_id varchar(20), post_id SERIAL, content text, created_at timestamp default now(), primary key(id), foreign key (user_id) references users on delete cascade, foreign key (post_id) references post on delete cascade);')
-cur.execute('CREATE TABLE buy (id SERIAL, user_id varchar(20), post_id SERIAL, created_at timestamp default now(), state varchar(10) check (state in (\'is_posted\', \'is_waiting\', \'is_done\')), sending_method varchar(10) check (sending_method in (\'delivery\', \'receipt\')),primary key(id), foreign key (user_id) references users on delete cascade, foreign key (post_id) references post on delete cascade);')
-cur.execute('CREATE TABLE post_like (id SERIAL, user_id varchar(20), post_id SERIAL, primary key(id), foreign key (user_id) references users on delete cascade, foreign key (post_id) references post on delete cascade);')
-cur.execute('CREATE TABLE user_like (id SERIAL, user_id varchar(20), nice_user_id varchar(20), primary key(id), foreign key (user_id) references users on delete cascade, foreign key (nice_user_id) references users on delete cascade);')
-connect.commit()
+# cur.execute('CREATE TABLE users (id varchar(20), password varchar(20), phone numeric(11, 0), phone_public boolean, primary key(id));')
+# cur.execute('CREATE TABLE post (id SERIAL, user_id varchar(20), title varchar(20) not null, content text, location text, image text, created_at timestamp default now(), updated_at timestamp default now(), is_closed boolean, primary key(id), foreign key (user_id) references users on delete cascade);')
+# cur.execute('CREATE TABLE comment (id SERIAL, user_id varchar(20), post_id SERIAL, content text, created_at timestamp default now(), primary key(id), foreign key (user_id) references users on delete cascade, foreign key (post_id) references post on delete cascade);')
+# cur.execute('CREATE TABLE buy (id SERIAL, user_id varchar(20), post_id SERIAL, created_at timestamp default now(), state varchar(10) check (state in (\'is_posted\', \'is_waiting\', \'is_done\')), sending_method varchar(10) check (sending_method in (\'delivery\', \'receipt\')),primary key(id), foreign key (user_id) references users on delete cascade, foreign key (post_id) references post on delete cascade);')
+# cur.execute('CREATE TABLE post_like (id SERIAL, user_id varchar(20), post_id SERIAL, primary key(id), foreign key (user_id) references users on delete cascade, foreign key (post_id) references post on delete cascade);')
+# cur.execute('CREATE TABLE user_like (id SERIAL, user_id varchar(20), nice_user_id varchar(20), primary key(id), foreign key (user_id) references users on delete cascade, foreign key (nice_user_id) references users on delete cascade);')
+# connect.commit()
 
 
 # 메인 랜딩 페이지
@@ -69,6 +70,34 @@ def signup():
 
     return render_template('users/signup.html')
 
+# post: 게시글 작성 기능
+@app.route('/post/create', methods=['GET', 'POST'])
+def post_create():
+    if request.method == 'POST':
+        if 'id' in session:
+            user_id = session['id']
+            title = request.form['title']
+            content = request.form['content']
+            location = request.form['location']
+            image = request.files['image']
+            image.save('static/uploads/' + secure_filename(image.filename))
+
+            is_closed = False
+            cur.execute('INSERT INTO post (user_id, title, content, location, image, is_closed) VALUES (\'{}\', \'{}\', \'{}\', \'{}\', \'{}\', \'{}\') RETURNING id;'.format(user_id, title, content, location, 'uploads/'+secure_filename(image.filename), is_closed))
+            connect.commit()
+
+            post_id = cur.fetchall()
+            return redirect(url_for('post_detail', id = post_id[0]))
+
+    return render_template('post/post_create.html')
+
+# post: 특정 게시글 보기 기능
+@app.route('/post/<id>', methods=['GET'])
+def post_detail(id):
+    cur.execute('SELECT * FROM post WHERE id = \'{}\';'.format(id))
+    post = cur.fetchall()
+
+    return render_template('post/post_detail.html', post = post[0])
 
 if __name__ == '__main__':
     app.run()
