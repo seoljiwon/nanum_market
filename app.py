222f82314593c2730054840fb9c5bc659192e3dc
seol <suljiwon@naver.com>
2021-12-10 18:59:28 +0900

    fix: revise word 'state' to 'status'

--- app.py
+++ app.py
@@ -18,20 +18,20 @@ connect = psycopg2.connect(DB_CONFIG)
 cur = connect.cursor()
 
 # 테이블 생성
-# cur.execute('DROP TABLE user_like;')
-# cur.execute('DROP TABLE post_like;')
-# cur.execute('DROP TABLE comment;')
-# cur.execute('DROP TABLE buy;')
-# cur.execute('DROP TABLE post;')
-# cur.execute('DROP TABLE users;')
+cur.execute('DROP TABLE user_like;')
+cur.execute('DROP TABLE post_like;')
+cur.execute('DROP TABLE comment;')
+cur.execute('DROP TABLE buy;')
+cur.execute('DROP TABLE post;')
+cur.execute('DROP TABLE users;')
 
-# cur.execute('CREATE TABLE users (id varchar(20), password varchar(20), phone numeric(11, 0), phone_public boolean, primary key(id));')
-# cur.execute('CREATE TABLE post (id SERIAL, user_id varchar(20), title varchar(20) not null, content text, location text, image text, created_at timestamp default now(), updated_at timestamp default now(), is_closed boolean, primary key(id), foreign key (user_id) references users on delete cascade);')
-# cur.execute('CREATE TABLE comment (id SERIAL, user_id varchar(20), post_id SERIAL, content text, created_at timestamp default now(), primary key(id), foreign key (user_id) references users on delete cascade, foreign key (post_id) references post on delete cascade);')
-# cur.execute('CREATE TABLE buy (id SERIAL, user_id varchar(20), post_id SERIAL, created_at timestamp default now(), state varchar(10) check (state in (\'is_posted\', \'is_waiting\', \'is_done\')), sending_method varchar(10) check (sending_method in (\'delivery\', \'receipt\')),primary key(id), foreign key (user_id) references users on delete cascade, foreign key (post_id) references post on delete cascade);')
-# cur.execute('CREATE TABLE post_like (id SERIAL, user_id varchar(20), post_id SERIAL, primary key(id), foreign key (user_id) references users on delete cascade, foreign key (post_id) references post on delete cascade);')
-# cur.execute('CREATE TABLE user_like (id SERIAL, user_id varchar(20), nice_user_id varchar(20), primary key(id), foreign key (user_id) references users on delete cascade, foreign key (nice_user_id) references users on delete cascade);')
-# connect.commit()
+cur.execute('CREATE TABLE users (id varchar(20), password varchar(20), phone numeric(11, 0), phone_public boolean, primary key(id));')
+cur.execute('CREATE TABLE post (id SERIAL, user_id varchar(20), title varchar(20) not null, content text, location text, image text, created_at timestamp default now(), updated_at timestamp default now(), is_closed boolean, primary key(id), foreign key (user_id) references users on delete cascade);')
+cur.execute('CREATE TABLE comment (id SERIAL, user_id varchar(20), post_id SERIAL, content text, created_at timestamp default now(), primary key(id), foreign key (user_id) references users on delete cascade, foreign key (post_id) references post on delete cascade);')
+cur.execute('CREATE TABLE buy (id SERIAL, user_id varchar(20), post_id SERIAL, created_at timestamp default now(), status varchar(10) check (status in (\'is_posted\', \'is_waiting\', \'is_done\')), sending_method varchar(10) check (sending_method in (\'delivery\', \'receipt\')),primary key(id), foreign key (user_id) references users on delete cascade, foreign key (post_id) references post on delete cascade);')
+cur.execute('CREATE TABLE post_like (id SERIAL, user_id varchar(20), post_id SERIAL, primary key(id), foreign key (user_id) references users on delete cascade, foreign key (post_id) references post on delete cascade);')
+cur.execute('CREATE TABLE user_like (id SERIAL, user_id varchar(20), nice_user_id varchar(20), primary key(id), foreign key (user_id) references users on delete cascade, foreign key (nice_user_id) references users on delete cascade);')
+connect.commit()
 
 
 # 메인 랜딩 페이지
@@ -212,9 +212,9 @@ def post_buy(id):
         if 'id' in session:
             user_id = session['id']
             sending_method = request.form['sending_method']
-            state = 'is_posted'
+            status = 'is_posted'
 
-            cur.execute('INSERT INTO buy (post_id, user_id, state, sending_method) VALUES (\'{}\', \'{}\', \'{}\', \'{}\') RETURNING id;'.format(id, user_id, state, sending_method))
+            cur.execute('INSERT INTO buy (post_id, user_id, status, sending_method) VALUES (\'{}\', \'{}\', \'{}\', \'{}\') RETURNING id;'.format(id, user_id, status, sending_method))
             connect.commit()
 
             buy_id = cur.fetchall()
@@ -228,9 +228,9 @@ def post_buy_request(id):
     if request.method == 'POST':
         if 'id' in session:
             user_id = session['id']
-            state = request.form['state']
+            status = request.form['status']
             
-            cur.execute('UPDATE buy SET state = \'{}\' WHERE post_id = \'{}\' and user_id = \'{}\';'.format(state, id, user_id))
+            cur.execute('UPDATE buy SET status = \'{}\' WHERE post_id = \'{}\' and user_id = \'{}\';'.format(status, id, user_id))
             connect.commit()
 
             return redirect(url_for('post_buy_request_waiting', id = id))
